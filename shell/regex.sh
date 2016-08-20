dad54215687eaeda2702b967c5963b00a561e9ef
lleolee <ll_501@163.com>
2016-08-20 22:05:46 +0800

    backup

--- shell/regex.sh
+++ shell/regex.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #file:regex.sh
 #function:test regular expression
+
+
+#regex mode
+
+#word ( ?[a-zA-zA-z]+ ?)
+#ip address [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}
+#ip address [:digit:]{1,3}\.[:digit:]{1,3}\.[:digit:]{1,3}\.[:digit:]{1,3}
+
