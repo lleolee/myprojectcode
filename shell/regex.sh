71504dc2a5b1ef8228f98bf54c4d4d7fbdc07c4f
lleolee <ll_501@163.com>
2016-08-26 22:24:17 +0800

    add grep cmd

--- shell/regex.sh
+++ shell/regex.sh
@@ -9,3 +9,20 @@
 #ip address [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}
 #ip address [:digit:]{1,3}\.[:digit:]{1,3}\.[:digit:]{1,3}\.[:digit:]{1,3}
 
+if [ $# -ne 2 ];
+then
+	echo "$0 match_text filename"
+	exit 0
+fi
+
+match_text=$1
+filename=$2
+
+grep -q $match_text $filename
+
+if [ $? -eq 0 ];
+then
+	echo "the text exist in the file"
+else
+	echo "the text does not exist in the file"
+fi
